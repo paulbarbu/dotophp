254b4731c9baa51fb7ef6c0d67f2c61cde7fba41
Paul Barbu <paullik@yahoo.com>
2011-08-27 00:04:17 +0300

    expiry query

--- docs/demo-queries.sql
+++ docs/demo-queries.sql
@@ -28,4 +28,39 @@ SELECT 'Deleting john' AS '';
 
 DELETE FROM user WHERE nick='joNny';
 
+SELECT 'Adding category: birthdays' AS ' ';
+
+INSERT INTO category(id, name, description, repeat_interval, color) 
+    VALUES(1, 'Birthdays', "My relative's birthdays", 1, 16777215);
+
+SELECT 'Adding category: other' AS ' ';
+
+INSERT INTO category(id, name, description, repeat_interval, color) 
+    VALUES(1, 'Other', "Stuff & Things", 1, 1777215);
+
+SELECT 'Adding \'Mom\' into Birthdays' AS ' ';
+
+INSERT INTO event(category_id, name, priority, repeat_interval, private, start, color) 
+    VALUES(1, 'Mom', 5, 4, 1, '2011-11-11 00:06:00',255);
+
+SELECT 'Setting alarm for mom' AS ' ';
+
+INSERT INTO alarm(id, date) VALUES(1, '2011-11-11 00:08:00');
+
+DELETE FROM category WHERE name='Birthdays';
+
+BEGIN;
+
+INSERT INTO user (nick, first_name, name, email, password, security_q, answer, private, phone, birthday, sex, tz, country, city, created)
+    VALUES('Foo', 'Bar', 'FooBar', 'barfoo@gmail.com', SHA1('asdf'),
+        'Unde locuiesti?', 'La bloc', 1, '0740000000', '1989-03-11 13:00:00', 'M', 'Romania/Bucharest', 'RO', 'Iasi', '2011-05-11 00:00:00');
+
+INSERT INTO pending (id, code) VALUES(LAST_INSERT_ID(), 'ab42ab42ba');
+
+COMMIT;
+
+SELECT 'Deleting un-activated user accounts(7 days passed from the creation without activation)' AS ' ';
+
+DELETE FROM user WHERE activated='0000-00-00 00:00:00' AND (UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(created))/604799 >= 1;
+
 SELECT '!!!FIREWORKS!!!' AS ' ';
