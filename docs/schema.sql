5587d1b4c2ada832cfaa57e4ee089b5072049a3c
Paul Barbu <paullik@yahoo.com>
2011-08-30 21:18:01 +0300

    fields renamed and reordered

--- docs/schema.sql
+++ docs/schema.sql
@@ -23,10 +23,10 @@ DROP TABLE IF EXISTS `alarm`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `alarm` (
-  `id` int(10) unsigned NOT NULL,
+  `event_id` int(10) unsigned NOT NULL,
   `date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  KEY `id` (`id`),
-  CONSTRAINT `alarm_ibfk_1` FOREIGN KEY (`id`) REFERENCES `event` (`event_id`) ON DELETE CASCADE
+  KEY `event_id` (`event_id`),
+  CONSTRAINT `alarm_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `event` (`event_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -70,15 +70,15 @@ DROP TABLE IF EXISTS `category`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `category` (
-  `id` mediumint(8) unsigned NOT NULL,
+  `category_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` mediumint(8) unsigned NOT NULL,
   `name` varchar(20) NOT NULL,
   `description` varchar(100) DEFAULT NULL,
-  `category_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `repeat_interval` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `color` mediumint(8) unsigned NOT NULL,
   PRIMARY KEY (`category_id`),
-  KEY `id` (`id`),
-  CONSTRAINT `category_ibfk_1` FOREIGN KEY (`id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+  KEY `id` (`user_id`),
+  CONSTRAINT `category_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -99,17 +99,17 @@ DROP TABLE IF EXISTS `event`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `event` (
+  `event_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `category_id` int(10) unsigned NOT NULL,
   `name` varchar(20) NOT NULL,
   `description` varchar(100) DEFAULT NULL,
-  `priority` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `event_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `repeat_interval` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `color` mediumint(8) unsigned NOT NULL,
+  `priority` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `private` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `exception` tinyint(1) unsigned NOT NULL DEFAULT '0',
   `start` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `end` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `color` mediumint(8) unsigned NOT NULL,
-  `exception` tinyint(1) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`event_id`),
   KEY `category_id` (`category_id`),
   CONSTRAINT `event_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE CASCADE
@@ -133,12 +133,12 @@ DROP TABLE IF EXISTS `friends`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `friends` (
-  `id` mediumint(8) unsigned NOT NULL,
-  `friend_id` mediumint(8) unsigned NOT NULL,
-  KEY `id` (`id`),
-  KEY `friend_id` (`friend_id`),
-  CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `friends_ibfk_2` FOREIGN KEY (`friend_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+  `initiator_id` mediumint(8) unsigned NOT NULL,
+  `followee_id` mediumint(8) unsigned NOT NULL,
+  KEY `initiator_id` (`initiator_id`),
+  KEY `followee_id` (`followee_id`),
+  CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`initiator_id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `friends_ibfk_2` FOREIGN KEY (`followee_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -159,10 +159,10 @@ DROP TABLE IF EXISTS `pending`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pending` (
-  `id` mediumint(8) unsigned NOT NULL,
+  `user_id` mediumint(8) unsigned NOT NULL,
   `code` char(10) NOT NULL,
-  KEY `id` (`id`),
-  CONSTRAINT `pending_ibfk_1` FOREIGN KEY (`id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `pending_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -184,28 +184,27 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `nick` varchar(20) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `first_name` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `nick` varchar(20) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` char(40) NOT NULL,
   `security_q` varchar(255) NOT NULL,
-  `answer` varchar(255) NOT NULL,
+  `security_a` varchar(255) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `activated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `private` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `phone` varchar(20) DEFAULT NULL,
-  `birthday` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `avatar` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `sex` enum('M','F') NOT NULL,
   `tz` varchar(27) NOT NULL,
   `country` enum('AF','AX','AL','DZ','AS','AD','AO','AI','AQ','AG','AR','AM','AW','AU','AT','AZ','BS','BH','BD','BB','BY','BE','BZ','BJ','BM','BT','BO','BQ','BA','BW','BV','BR','IO','BN','BG','BF','BI','KH','CM','CA','CV','KY','CF','TD','CL','CN','CX','CC','CO','KM','CG','CD','CK','CR','CI','HR','CU','CW','CY','CZ','DK','DJ','DM','DO','EC','EG','SV','GQ','ER','EE','ET','FK','FO','FJ','FI','FR','GF','PF','TF','GA','GM','GE','DE','GH','GI','GR','GL','GD','GP','GU','GT','GG','GN','GW','GY','HT','HM','VA','HN','HK','HU','IS','IN','ID','IR','IQ','IE','IM','IL','IT','JM','JP','JE','JO','KZ','KE','KI','KP','KR','KW','KG','LA','LV','LB','LS','LR','LY','LI','LT','LU','MO','MK','MG','MW','MY','MV','ML','MT','MH','MQ','MR','MU','YT','MX','FM','MD','MC','MN','ME','MS','MA','MZ','MM','NA','NR','NP','NL','NC','NZ','NI','NE','NG','NU','NF','MP','NO','OM','PK','PW','PS','PA','PG','PY','PE','PH','PN','PL','PT','PR','QA','RE','RO','RU','RW','BL','SH','KN','LC','MF','PM','VC','WS','SM','ST','SA','SN','RS','SC','SL','SG','SX','SK','SI','SB','SO','ZA','GS','SS','ES','LK','SD','SR','SJ','SZ','SE','CH','SY','TW','TJ','TZ','TH','TL','TG','TK','TO','TT','TN','TR','TM','TC','TV','UG','UA','AE','GB','US','UM','UY','UZ','VU','VE','VN','VG','VI','WF','EH','YE','ZM','ZW') NOT NULL,
   `city` varchar(30) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `birthday` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `avatar` varchar(255) DEFAULT NULL,
+  `sex` enum('M','F') NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nick` (`nick`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `nick_2` (`nick`)
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -227,4 +226,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-08-29 21:31:23
+-- Dump completed on 2011-08-30 19:48:14
