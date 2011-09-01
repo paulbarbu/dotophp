6dd8cc0046ac1d97f97b60685847a7f0fb4b5f1c
Paul Barbu <paullik@yahoo.com>
2011-09-01 16:13:33 +0300

    matching queries with new fields

--- docs/demo-queries.sql
+++ docs/demo-queries.sql
@@ -5,8 +5,8 @@ SELECT 'Adding John as a new user, using transactions' AS ' ';
 BEGIN;
 
 INSERT INTO user (first_name, last_name, description, nick, email, password, security_q, security_a, private, tz, country, city, phone, birthday, sex)
-    VALUES('jonny', 'John', 'Gates', 'john@mail.com', SHA1('asdf42'),
-        'Cum o cheama pe sora ta?', 'Emily', 1, '0742424242', '1980-05-21 15:00:00', 'Eu sunt romanul John din Arabia.', 'M', 'Romania/Bucharest', 'RO', 'Sibiu');
+    VALUES('John', 'Gates', 'Eu sunt romanul John din Arabia.', 'jonny', 'john@mail.com', SHA1('asdf42'),
+        'Cum o cheama pe sora ta?', 'Emily', 1, 'Romania/Bucharest', 'RO', 'Sibiu', '0742424242', '1980-05-21 15:00:00', 'M');
 
 INSERT INTO pending (user_id, code) VALUES(LAST_INSERT_ID(), 'ab42ab42ba');
 
@@ -16,8 +16,8 @@ SELECT 'Adding john as a new user, using transactions' AS ' ';
 
 BEGIN;
 INSERT INTO user (first_name, last_name, description, nick, email, password, security_q, security_a, private, tz, country, city, phone, birthday, sex)
-    VALUES('joNny', 'John', 'Gates', 'john@gmail.com', SHA1('asdf42'),
-        'Cum o cheama pe sora ta?', 'Emily', 1, '0742424242', '1980-05-21 15:00:00', 'Eu sunt romanul John din Arabia.', 'M', 'Romania/Bucharest', 'RO', 'Sibiu');
+    VALUES('John', 'Gates', 'Eu sunt romanul John din Arabia.', 'joNny', 'john@gmail.com', SHA1('asdf42'),
+        'Cum o cheama pe sora ta?', 'Emily', 1, 'Romania/Bucharest', 'RO', 'Sibiu', '0742424242', '1980-05-21 15:00:00', 'M');
 
 INSERT INTO pending (user_id, code) VALUES(LAST_INSERT_ID(), 'ab42ab42ba');
 
@@ -34,13 +34,13 @@ INSERT INTO category(user_id, name, description, repeat_interval, color)
 
 SELECT 'Adding category: other' AS ' ';
 
-INSERT INTO category(user_id, name, description, repeat_interval, color) 
+INSERT INTO category(user_id, name, description, repeat_interval, color)
     VALUES(1, 'Other', "Stuff & Things", 1, 1777215);
 
 SELECT 'Adding \'Mom\' into Birthdays' AS ' ';
 
 INSERT INTO event(category_id, name, repeat_interval,  color, priority, private, start)
-    VALUES(1, 'Mom', 5, 4, 1, '2011-11-11 00:06:00',255);
+    VALUES(1, 'Mom', 5, 255,4, 1, '2011-11-11 00:06:00');
 
 SELECT 'Setting alarm for mom' AS ' ';
 
@@ -52,7 +52,7 @@ BEGIN;
 
 INSERT INTO user (first_name, last_name, nick, email, password, security_q, security_a, created, private, tz, country, city, phone, birthday, sex)
     VALUES('Foo', 'Bar', 'FooBar', 'barfoo@gmail.com', SHA1('asdf'),
-        'Unde locuiesti?', 'La bloc', 1, '0740000000', '1989-03-11 13:00:00', 'M', 'Romania/Bucharest', 'RO', 'Iasi', '2011-05-11 00:00:00');
+        'Unde locuiesti?', 'La bloc', '2011-05-11 00:00:00', 1, 'Romania/Bucharest', 'RO', 'Iasi', '0740000000', '1989-03-11 13:00:00', 'M');
 
 INSERT INTO pending (user_id, code) VALUES(LAST_INSERT_ID(), 'ab42ab42ba');
 
@@ -70,7 +70,7 @@ UPDATE user SET avatar='srv/http/avatars/img1.png', description='Me, jonny!' WHE
 SELECT 'New \'Homework\' event!' AS ' ';
 
 INSERT INTO event(category_id, name, repeat_interval,  color, priority, private, exception, start, end)
-    VALUES(2, 'Homework', 10, 1, 0, '2011-09-15 09:00:00', '2012-06-15 09:00:00', 255, 1);
+    VALUES(2, 'Homework', 1, 255, 1, 0, 1, '2011-09-15 09:00:00', '2012-06-15 09:00:00');
 
 SELECT 'Setting one alarm for \'Homework\' event!' AS ' ';
 
