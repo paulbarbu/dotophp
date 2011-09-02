983f0effe2817215f3520f169d60528869373b74
Paul Barbu <paullik@yahoo.com>
2011-09-02 17:53:38 +0300

    category and event function sigs

--- src/agenda/functions.php
+++ src/agenda/functions.php
@@ -11,13 +11,13 @@
 /**
  * Adds a new event
  *
- * @param unsigned int $cat_id the category where the event will be created
- * @param string $name event's name, max length: 20 characters
+ * @param int $cat_id [1, 2^32) the category where the event will be created
+ * @param string $name event's name, min. length: 1 char, max length: 20 characters
  * @param string $desc event description, default NULL, max length: 100 chars
  * @param int $repeat_interval the interval the event should repeat itself,
- * allowed values: TODO values, default: 0
- * @param unsigned int $color color code, base 10 representation
- * @param unsigned int $priority higher numbers mean higher priority, default: 0
+ * allowed values: {0, 1, 2, 3, 4} (Never, Daily, Weekly, Monthly, Yearly), default: 0
+ * @param int $color [1, 2^16) color code, base 10 representation
+ * @param int $priority {0, 1, 2, 3} (Low, Medium, High, Urgent), default: 0
  * @param int $private allowed values: 1(private) or 0(public), default: 0
  * @param int $exception allowed values: 1(exception) or 0(normal), default: 0
  * @param int $start timestamp representing the date and time the event should
@@ -39,7 +39,20 @@ function addEvent($cat_id, $name, $desc = NULL, $repeat_interval = 0, $color = N
  * Changes an event's properties that are passed to the function as being NOT
  * NULL
  *
- * @param unsigned int $event_id the event's ID the user itneds to update
+ * @param int $event_id [1, s^32) the event's ID the user itneds to update
+ * @param int $cat_id [1, 2^32) the category where the event will be created
+ * @param string $name event's name, min. length: 1 char, max length: 20 characters
+ * @param string $desc event description, default NULL, max length: 100 chars
+ * @param int $repeat_interval the interval the event should repeat itself,
+ * allowed values: {0, 1, 2, 3, 4} (Never, Daily, Weekly, Monthly, Yearly), default: 0
+ * @param int $color [1, 2^16) color code, base 10 representation
+ * @param int $priority {0, 1, 2, 3} (Low, Medium, High, Urgent), default: 0
+ * @param int $private allowed values: 1(private) or 0(public), default: 0
+ * @param int $exception allowed values: 1(exception) or 0(normal), default: 0
+ * @param int $start timestamp representing the date and time the event should
+ * start
+ * @param int $end timestamp representing the date and time the event should
+ * end
  *
  * @return an array consisting of a BOOL value and a NULL or the error string,
  * array(BOOL, string)
@@ -54,16 +67,55 @@ function updateEvent($event_id, $cat_id = NULL, $name = NULL, $desc = NULL,
 /**
  * Deletes an event
  *
- * @param unsigned int $event_id the event's ID the user intends to delete
+ * @param int $event_id [1, 2^32) the event's ID the user intends to delete
  */
-function delEvent(){
+function delEvent($event_id){
 }
 
-function addCat(){
+/**
+ * Add a new category
+ *
+ * @param int $user_id [1, 2^16) the user who creates the category
+ * @param string $name category's name, min. length: 1 char, max. length: 20
+ * chars
+ * @param string $description category's description, max. length: 100 chars,
+ * default: NULL
+ * @param int $repeat_interval the interval the event should repeat itself,
+ * allowed values: {0, 1, 2, 3, 4} (Never, Daily, Weekly, Monthly, Yearly), default: 0
+ * @param int $color [1, 2^16) color code, base 10 representation
+ *
+ * @return an array consisting of a BOOL value and a NULL or the error string,
+ * array(BOOL, string)
+ */
+function addCat($user_id, $name, $description = NULL, $repeat_interval = 0,
+    $color = NULL
+    ){
 }
 
-function updateCat(){
+/**
+ * Update a category
+ *
+ * @param int $category_id [1, 2^32) the category's ID that should be modified
+ * @param string $name category's name, min. length: 1 char, max. length: 20
+ * chars
+ * @param string $description category's description, max. length: 100 chars,
+ * default: NULL
+ * @param int $repeat_interval the interval the event should repeat itself,
+ * allowed values: {0, 1, 2, 3, 4} (Never, Daily, Weekly, Monthly, Yearly), default: 0
+ * @param int $color [1, 2^16) color code, base 10 representation
+ */
+function updateCat($category_id, $name = NULL, $description = NULL,
+    $repeat_interval = NULL, $color = NULL
+    ){
 }
 
-function delCat(){
+/**
+ * Delete a category
+ *
+ * @param int $category_id category's id to be deleted
+ *
+ * @return an array consisting of a BOOL value and a NULL or the error string,
+ * array(BOOL, string)
+ */
+function delCat($category_id){
 }
