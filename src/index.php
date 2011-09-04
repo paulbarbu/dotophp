48cb8dee7d0fb536ad309b19754a632bdd870004
Paul Barbu <paullik@yahoo.com>
2011-09-04 14:15:46 +0300

    overwriting some constants

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<?php
+
+define('MODULES_ROOT', '/media/PauLLiK/localhost/github/dotophp/src/');
+define('LAYOUT_PATH', __DIR__ . DIRECTORY_SEPARATOR . 'layout.php');
+
+$modules = require '/media/PauLLiK/localhost/github/dotophp/src/modules.php';
+
+include '/media/PauLLiK/localhost/github/dotophp/src/global_functions.php';
+
+include '/media/PauLLiK/localhost/github/yaCMS/src/index.php';
+
