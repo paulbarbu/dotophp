3bb5d0788437d19c88cff0f3e509463b22967643
Paul Barbu <paullik@yahoo.com>
2011-09-07 10:34:11 +0300

    activation module

---
+++ src/activate/content.php
@@ -0,0 +1,12 @@
+<form method="post" action="" >
+<table border=0 cellspacing=5>
+<tr><td>
+<label for="code">Activation code:</label></td><td><input type="text" maxlength=10 name="code" id="code" />
+</tr></td><tr><td>
+<label for"pass">Password:</label></td><td><input type="password" maxlength="30" id="pass" name="pass" />
+</tr></td><tr><td>
+<label for="passconfirm">Confirm password:</label></td><td><input type="password" maxlength="30" id="passconfirm" name="passconfirm" />
+</tr></td><tr><td>
+</table>
+<input type="submit" value="Activate!" name="activate" />
+</form>
---
+++ src/activate/functions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file src/activate/functions.php
+ * @brief Functions for activate module
+ * @author Paul Barbu
+ *
+ * @ingroup activateFiles
+ */
+
+/**
+ * @defgroup activateFiles Activate files
+ */
+
+/**
+ * Compare the user's activation code with the activation code in the DB
+ *
+ * @param string $acode user supplied activation code
+ *
+ * @return BOOL TRUE if the strings match, else, FALSE
+ */
+function isValidACode($acode){
+    //GAP
+}
