92da972bdbb8aac1cae0fa08c31b6ad02bf3138d
Paul Barbu <paullik@yahoo.com>
2011-09-09 23:07:24 +0300

    [doc]added docs for index file

--- src/index.php
+++ src/index.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * @file src/index.php
+ * @brief Index file
+ * @author Paul Barbu
+ *
+ * @ingroup globalFiles
+ *
+ * Every custom thing must be overwritten here in order to be recognized by
+ * yaCMS
+ */
 
 define('MODULES_ROOT', '/media/PauLLiK/localhost/github/dotophp/src/');
 define('LAYOUT_PATH', __DIR__ . DIRECTORY_SEPARATOR . 'layout.php');
