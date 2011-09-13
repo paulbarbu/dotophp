dcf616a52cd3d62026b929f6dd0523e1532f7c87
Paul Barbu <paullik@yahoo.com>
2011-09-13 18:27:02 +0300

    finished sketching auth module

--- src/authentification/auth.php
+++ src/authentification/auth.php
@@ -5,12 +5,11 @@
  * @author Paul Barbu
  *
  * @ingroup authFiles
+ *
+ * autologin
+ *
+ * if not logged in, login
+ * if logged in, logout
+ *
+ * send the feedback to the VL, labeled for the logout or for the login state
  */
-
-//autologin
-//
-//
-//if not logged in, login
-//if logged in, logout
-//
-//send the feedback to the VL, labeled for the logout or for the login state
---
+++ src/authentification/content_auth.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file src/authentification/content_auth.php
+ * @brief HTML file for the authentification module
+ * @author Paul Barbu
+ *
+ * @ingroup authFiles
+ *
+ * Here will be requested content_login.php or content_logout.php
+ * (depending on the user's state - logged in/out)
+ */
+
+?>
