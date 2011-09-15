9b355e9d5ad9ba16f42dc6261f92555c0531df6b
Paul Barbu <paullik@yahoo.com>
2011-09-15 20:15:05 +0300

    show=auth not show=login

--- src/home/content.php
+++ src/home/content.php
@@ -1,1 +1,1 @@
-<p><a href="index.php?show=register">Register</a> or <a href="index.php?show=login">Log in</a></p>
+<p><a href="index.php?show=register">Register</a> or <a href="index.php?show=auth">Log in</a></p>
