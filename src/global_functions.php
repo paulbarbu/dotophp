7638cb353aaf456d89d9e5e395fb0c042cc346b2
Paul Barbu <paullik@yahoo.com>
2011-09-30 22:29:37 +0300

    source of the email regex

--- src/global_functions.php
+++ src/global_functions.php
@@ -97,6 +97,7 @@ function isValidNick($nick){
  * @return BOOL TRUE if the email is valid, else FALSE
  */
 function isValidMail($email){
+    //thanks to: http://www.regular-expressions.info/email.html
     return (bool)preg_match("/[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/", $email);
 }
 
