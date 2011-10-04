e1b583ef111424e186b53b6194261b0f2554705d
Paul Barbu <paullik@yahoo.com>
2011-10-04 21:55:38 +0300

    checking the captcha entered by the user

--- src/register/register.php
+++ src/register/register.php
@@ -8,7 +8,15 @@
  */
 
 if(isset($_POST['register'])){
-    //TODO: check captcha then call addUser and is it gives an error then report
-    //it to VL
-    //GAP
+    //TODO: check captcha then call addUser and is it gives an error then report it to view
+    //add pending account, send mail with activation code
+
+    if(isValidCaptcha($_SESSION['last_captcha'], $_POST['captcha'])){
+        var_dump("hello");
+        //TODO: proceed registration
+    }
+    else{
+        var_dump("CAPTCHA");
+        //TODO: invent ERR_CAPTCHA and return it to the view
+    }
 }
