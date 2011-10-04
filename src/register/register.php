2793e3057be0f20b25616404145c940a0923f39e
Paul Barbu <paullik@yahoo.com>
2011-10-04 22:42:43 +0300

    connecting to the db and calling addUser(), new todo points

--- src/register/register.php
+++ src/register/register.php
@@ -7,13 +7,31 @@
  * @ingroup registerFiles
  */
 
+var_dump($_SESSION['captcha']);
+
 if(isset($_POST['register'])){
     //TODO: check captcha then call addUser and is it gives an error then report it to view
     //add pending account, send mail with activation code
 
     if(isValidCaptcha($_SESSION['last_captcha'], $_POST['captcha'])){
-        var_dump("hello");
         //TODO: proceed registration
+        $link = mysqli_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DATABASE);
+
+        if(!$link){
+            die('Connect error (' . mysqli_connect_errno() . '): ' . mysqli_connect_error());
+            //TODO: ERR_DB_CONNECTION
+        }
+
+        addUser($link, $_POST['first_name'], $_POST['last_name'], $_POST['nick'],
+            $_POST['email'], isset($_POST['private']) ? 1 : 0, $_POST['timezone'], $_POST['country'],
+            $_POST['city'], $_POST['sex'], $_POST['description'], $_POST['phone'],
+            $_POST['birthday']);
+
+        //TODO: check the return value of addUser and take action accordignly
+
+        mysqli_close($link);
+
+        //TODO: do mail here
     }
     else{
         var_dump("CAPTCHA");
