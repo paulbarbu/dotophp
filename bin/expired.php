2ceb2ba59e7ebbbcc099f7bbe88ebbc1afeca1a9
Paul Barbu <paullik@yahoo.com>
2011-10-28 23:10:54 +0300

    logging on deletion too

--- bin/expired.php
+++ bin/expired.php
@@ -14,17 +14,18 @@
  * @defgroup otherFiles Other files
  */
 
+require '../src/global_functions.php';
 $feedback_pre['connect'] = require '../src/mysql/connect.php';
 
 if($feedback_pre['connect']){
     mysqli_query($feedback_pre['connect'],
         "DELETE FROM user WHERE activated='0000-00-00 00:00:00' AND (UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(created))/604799 >= 1;");
 
+    writeLog('../logs/expired.log', date('d.m.Y H:i:s') . ' - Expired accounts: ' . mysqli_affected_rows($feedback_pre['connect']) . PHP_EOL);
+
     require '../src/mysql/disconnect.php';
 }
 else{
-    require '../src/global_functions.php';
-
     writeLog('../logs/expired.log', date('d.m.Y H:i:s') . ' - Connection error: ('
         . mysqli_connect_errno() . ') ' . mysqli_connect_error() . PHP_EOL);
 }
