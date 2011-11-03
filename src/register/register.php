087bcb2d1f06e78ca892b920c7ead184925e774f
Barbu Paul - Gheorghe <paullik.paul@gmail.com>
2011-11-03 23:11:51 +0200

    rewritten register module to log the DB errors

--- src/register/register.php
+++ src/register/register.php
@@ -11,6 +11,7 @@ var_dump($_SESSION['captcha']);
 
 if(isset($_POST['register'])){
     $result = array();
+    $retval = NULL;
 
     list($captcha, $first_name, $last_name, $nick, $email, $city, $description,
         $phone, $birthday) = filterInput($_POST['captcha'], $_POST['first_name'],
@@ -21,66 +22,81 @@ if(isset($_POST['register'])){
     if(isValidCaptcha($_SESSION['last_captcha'], $captcha)){
 
         if(!$feedback_pre['connect']){
-            return R_ERR_DB_CONNECTION;
-        }
-
-        $existing_user = isUser($feedback_pre['connect'], $nick, $email);
-        if($existing_user == MATCHING_NICK){
-            return R_ERR_USED_NICK;
-        }
-        elseif($existing_user == MATCHING_MAIL){
-            return R_ERR_USED_MAIL;
-        }
-
-        isset($_POST['sex']) ? $sex = $_POST['sex'] : $sex = NULL;
-
-        if(!mysqli_query($feedback_pre['connect'], 'BEGIN;')){
-            return R_ERR_DB;
-        }
-
-        $result = addUser($feedback_pre['connect'], $first_name, $last_name, $nick,
-            $email, isset($_POST['private']) ? 1 : 0, $_POST['timezone'], $_POST['country'],
-            $city, $sex, $description, $phone, $birthday);
-
-        if(!$result[0]){
-            return $result[1];
+            $retval = R_ERR_DB_CONNECTION;
         }
         else{
-            $activation_code = genActivationCode($nick);
-
-            if(!addPendingUser($feedback_pre['connect'], $activation_code)){
-                return R_ERR_DB;
+            $existing_user = isUser($feedback_pre['connect'], $nick, $email);
+            if($existing_user == MATCHING_NICK){
+                $retval = R_ERR_USED_NICK;
             }
-
-            $mail_data = require 'mail_data.php';
-            $url = 'http://' . $_SERVER['SERVER_NAME'] . app_path() . '/index.php?show=activate';
-
-            $msg_specifiers = array(
-                'nick' => $nick,
-                'code_link' => $url . '&code=' . $activation_code,
-                'activation_link' => $url,
-                'code' => $activation_code,
-            );
-
-            if(!mail($email, vsprintf_named($mail_data['subject'], array('nick' => $nick)),
-                     vsprintf_named($mail_data['msg'], $msg_specifiers), $mail_data['header'])){
-
-                mysqli_query($feedback_pre['connect'], 'DELETE FROM pending WHERE user_id = LAST_INSERT_ID();');
-                mysqli_query($feedback_pre['connect'], 'DELETE FROM user WHERE id = LAST_INSERT_ID();');
-
-                return R_ERR_MAIL;
+            elseif($existing_user == MATCHING_MAIL){
+                $retval = R_ERR_USED_MAIL;
             }
+            else{
+                isset($_POST['sex']) ? $sex = $_POST['sex'] : $sex = NULL;
 
-            if(!mysqli_query($feedback_pre['connect'], 'COMMIT;')){
-                return R_ERR_DB;
+                if(!mysqli_query($feedback_pre['connect'], 'BEGIN;')){
+                    $retval = R_ERR_DB;
+                }
+                else{
+
+                    $result = addUser($feedback_pre['connect'], $first_name, $last_name, $nick,
+                        $email, isset($_POST['private']) ? 1 : 0, $_POST['timezone'], $_POST['country'],
+                        $city, $sex, $description, $phone, $birthday);
+
+                    if(!$result[0]){
+                        $retval = $result[1];
+                    }
+                    else{
+                        $activation_code = genActivationCode($nick);
+
+                        if(!addPendingUser($feedback_pre['connect'], $activation_code)){
+                            $retval = R_ERR_DB;
+                        }
+                        else{
+
+                            $mail_data = require 'mail_data.php';
+                            $url = 'http://' . $_SERVER['SERVER_NAME'] . app_path() . '/index.php?show=activate';
+
+                            $msg_specifiers = array(
+                                'nick' => $nick,
+                                'code_link' => $url . '&code=' . $activation_code,
+                                'activation_link' => $url,
+                                'code' => $activation_code,
+                            );
+
+                            if(!mail($email, vsprintf_named($mail_data['subject'], array('nick' => $nick)),
+                                     vsprintf_named($mail_data['msg'], $msg_specifiers), $mail_data['header'])){
+
+                                mysqli_query($feedback_pre['connect'], 'DELETE FROM pending WHERE user_id = LAST_INSERT_ID();');
+                                mysqli_query($feedback_pre['connect'], 'DELETE FROM user WHERE id = LAST_INSERT_ID();');
+
+                                $retval = R_ERR_MAIL;
+                            }
+                            else{
+
+                                if(!mysqli_query($feedback_pre['connect'], 'COMMIT;')){
+                                    $retval = R_ERR_DB;
+                                }
+                                else{
+                                    $retval = ERR_NONE;
+                                }
+                            }
+                        }
+                    }
+                }
             }
-
-            return ERR_NONE;
         }
     }
     else{
-        return R_ERR_CAPTCHA;
+        $retval = R_ERR_CAPTCHA;
     }
+
+    if($retval == R_ERR_DB || $retval == R_ERR_DB_CONNECTION){
+        writeLog('../logs/register.log', '(' . mysqli_errno($feedback_pre['connect']) . ') ' . mysqli_error($feedback_pre['connect']) . PHP_EOL);
+    }
+
+    return $retval;
 }
 
 return TRUE;
