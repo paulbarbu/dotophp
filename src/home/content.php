bbc7bd61c1a15d27c04f748be2112edee09e925a
Barbu Paul - Gheorghe <paullik.paul@gmail.com>
2011-11-04 22:43:32 +0200

    home page XHTML 1.0 Transitional validated

--- src/home/content.php
+++ src/home/content.php
@@ -1,1 +1,1 @@
-<p><a href="index.php?show=register">Register</a> or <a href="index.php?show=auth">Log in</a></p>
+<a href="index.php?show=register">Register</a> or <a href="index.php?show=auth">Log in</a>
