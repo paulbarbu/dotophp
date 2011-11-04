40f5b70f9dd59c8f7e4e0e3af629f5a9ea54e102
Barbu Paul - Gheorghe <paullik.paul@gmail.com>
2011-11-04 22:45:12 +0200

    the base pattern(the layout) XHTML 1.0 Transitional validated

--- src/layout.php
+++ src/layout.php
@@ -7,7 +7,7 @@
  */
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -31,9 +31,7 @@
                             . $modules[$module]['VL']['content']) &&
                 is_readable(MODULES_ROOT . $module . DIRECTORY_SEPARATOR
                 . $modules[$module]['VL']['content'])){
-        ?>
-            <p>
-        <?php
+
                     include MODULES_ROOT . $module . DIRECTORY_SEPARATOR
                         . $modules[$module]['VL']['content'];
             }
@@ -41,8 +39,6 @@
                 echo '<h3>' , ERR_LOAD_FILE , '</h3>';
             }
         ?>
-
-            </p>
     </div>
 </body>
 </html>
