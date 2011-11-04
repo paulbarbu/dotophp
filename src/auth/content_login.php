069a45432605f8ecf34f7a4f2aab20585905c86c
Barbu Paul - Gheorghe <paullik.paul@gmail.com>
2011-11-04 22:43:14 +0200

    login page XHTML 1.0 Transitional validated

--- src/auth/content_login.php
+++ src/auth/content_login.php
@@ -8,13 +8,13 @@
  */
 ?>
 <form action="" method="post">
-<table border=0 cellspacing=5>
+<table border="0" cellspacing="5">
 <tr><td>
-<label for="nick">Nickname:</label></td><td><input id="nick" type="text" name="nick" maxlength=20 />
+<label for="nick">Nickname:</label></td><td><input id="nick" type="text" name="nick" maxlength="20" />
 </td></tr><tr><td>
 <label for="pass">Password:</label></td><td><input type="password" maxlength="30" id="pass" name="pass" />
 </td></tr><tr><td>
-<label for="remember">Remember me</label></td><td><input type="checkbox" checked name="remember" id="remember" />
+<label for="remember">Remember me</label></td><td><input type="checkbox" checked="checked" name="remember" id="remember" />
 </td></tr>
 </table>
 <input type="submit" name="login" value="Log in" />
