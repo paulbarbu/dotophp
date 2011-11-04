ac7984f5b4a1ab829d322aecbd1b895c2253ffd1
Barbu Paul - Gheorghe <paullik.paul@gmail.com>
2011-11-04 22:42:40 +0200

    activate module XHTML 1.0 Transitional validated

--- src/activate/content.php
+++ src/activate/content.php
@@ -10,23 +10,23 @@
 if($feedback['activate']){
 ?>
 <form method="post" action="" >
-<table border=0 cellspacing=5>
+<table border="0" cellspacing="5">
 <tr><td>
-<label for="code">Activation code:</label></td><td><input type="text" maxlength=10
-name="code" id="code" <?php list($_GET['code']) = filterInput($_GET['code']);
+<label for="code">Activation code:</label></td><td><input type="text" maxlength="10"
+ name="code" id="code" <?php isset($_GET['code']) ? list($_GET['code']) = filterInput($_GET['code']) : NULL;
 echo isset($_GET['code']) && !empty($_GET['code']) ? 'value="' . $_GET['code'] . '" disabled' : 'tabindex="1"' ?> />
-</tr></td><tr><td>
+</td></tr><tr><td>
 <label for="pass">Password:</label></td><td><input tabindex="2" type="password" maxlength="30" id="pass" name="pass" />
-</tr></td><tr><td>
+</td></tr><tr><td>
 <label for="passconfirm">Confirm password:</label></td><td><input tabindex="3" type="password" maxlength="30" id="passconfirm" name="passconfirm" />
-</tr></td><tr><td>
+</td></tr><tr><td>
 <label for="question">Security question:</label></td><td><input tabindex="4" type="text" maxlength="255" id="question" name="security_q" />
-</tr></td><tr><td>
+</td></tr><tr><td>
 <label for="answer">Security answer:</label></td><td><input tabindex="5" type="text" maxlength="255" id="answer" name="security_a" />
-</tr></td><tr><td colspan="2"><center>
+</td></tr><tr><td colspan="2"><center>
 <input tabindex="6" type="submit" value="Activate!" name="activate" />
 <input tabindex="7" type="reset" name="reset" value="Reset" />
-</center></tr></td>
+</center></td></tr>
 </table>
 </form>
 <?php
