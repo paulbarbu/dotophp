19b23e55f1f7a213c3c55d4360db84b941d9c928
Barbu Paul - Gheorghe <paullik.paul@gmail.com>
2011-11-16 22:29:18 +0200

    tabindex-ed the elements on recover's form

--- src/recover/content.php
+++ src/recover/content.php
@@ -14,10 +14,11 @@
 <form action="" method="post">
 <table border="0" cellspacing="5">
 <tr><td>
-    <label for="nick">Nickname:</label></td><td><input id="nick" type="text" name="nick" maxlength="20" />
+    <label for="nick">Nickname:</label></td><td><input id="nick" type="text" name="nick" maxlength="20" tabindex="1" />
     </td></tr><tr><td>
-    <label for="email">E-mail:</label></td><td><input id="email" type="text" name="email" maxlength="255" />
-    </td></tr>
+    <label for="email">E-mail:</label></td><td><input id="email" type="text" name="email" maxlength="255" tabindex= "2" />
+    </td></tr><tr><td colspan="2" ><center>
+<input type="submit" name="recover" value="Recover" tabindex="3" /></center>
+</td></tr>
 </table>
-<input type="submit" name="recover" value="Recover" />
 </form>
