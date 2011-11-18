2035250f43c1a9d0b208c2a353445aff7a5be6f9
Barbu Paul - Gheorghe <paullik.paul@gmail.com>
2011-11-18 22:39:17 +0200

    finished first part of the recover module, where we're 'getting to know' the user

--- src/recover/content.php
+++ src/recover/content.php
@@ -10,15 +10,32 @@
 /**
  * @defgroup recoverFiles Recover module
  */
+
+if(is_numeric($feedback['recover']) && $feedback['recover'] == RECOVER_PROCESSED){
+    var_dump($_SESSION);
+    //TODO: Here be secutiry_* form
+}
+else{
 ?>
 <form action="" method="post">
 <table border="0" cellspacing="5">
 <tr><td>
-    <label for="nick">Nickname:</label></td><td><input id="nick" type="text" name="nick" maxlength="20" tabindex="1" />
-    </td></tr><tr><td>
     <label for="email">E-mail:</label></td><td><input id="email" type="text" name="email" maxlength="255" tabindex= "2" />
     </td></tr><tr><td colspan="2" ><center>
-<input type="submit" name="recover" value="Recover" tabindex="3" /></center>
+<input type="submit" name="proceed" value="Proceed" tabindex="3" /></center>
 </td></tr>
 </table>
 </form>
+<?php
+    if(is_numeric($feedback['recover'])){
+        echo '<h3>';
+
+        switch($feedback['recover']){
+            case RECOVER_ERR_NOUSER: printf("This email is not associated to any account! (#%d)", RECOVER_ERR_NOUSER);
+                break;
+            case RECOVER_ERR_INACTIVE: printf("The account you're trying to recover has not been activated yet! (#%d)", RECOVER_ERR_INACTIVE);
+                break;
+        }
+        echo '</h3>';
+    }
+}
--- src/recover/recover.php
+++ src/recover/recover.php
@@ -7,3 +7,33 @@
  * @ingroup recoverFiles
  */
 
+if(isset($_POST['proceed'])){
+    list($email) = filterInput($_POST['email']);
+
+    //if i find this email, if the account is activated, proceed to displaying
+    //the security question
+
+    if(isUser($feedback_pre['connect'], NULL, $email) !== MATCHING_MAIL){
+        return RECOVER_ERR_NOUSER;
+    }
+
+    $result = mysqli_query($feedback_pre['connect'],
+        "SELECT id, activated, security_q, security_a FROM user WHERE email = '"
+        . $email . "';");
+
+    $data = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+    if($data['activated'] == "0000-00-00 00:00:00"){
+        return RECOVER_ERR_INACTIVE;
+    }
+
+    $_SESSION['security_q'] = $data['security_q'];
+    $_SESSION['security_a'] = $data['security_a'];
+    $_SESSION['id'] = $data['id'];
+
+    return RECOVER_PROCESSED;
+}
+//else if the security answer has been submitted, check it and recover the
+//account
+
+return TRUE;
