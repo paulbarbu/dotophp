12ef8f32ac4e17cad5c2681ca98371900ba30e5f
Barbu Paul - Gheorghe <paullik.paul@gmail.com>
2011-11-26 20:30:50 +0200

    added tabindex on login view

--- src/auth/content_login.php
+++ src/auth/content_login.php
@@ -10,13 +10,13 @@
 <form action="" method="post">
 <table border="0" cellspacing="5">
 <tr><td>
-<label for="nick">Nickname:</label></td><td><input id="nick" type="text" name="nick" maxlength="20" />
+<label for="nick">Nickname:</label></td><td><input id="nick" type="text" name="nick" maxlength="20" tabindex="1" />
 </td></tr><tr><td>
-<label for="pass">Password:</label></td><td><input type="password" maxlength="30" id="pass" name="pass" />
+<label for="pass">Password:</label></td><td><input type="password" maxlength="30" id="pass" name="pass" tabindex="2"/>
 </td></tr><tr><td>
-<label for="remember">Remember me</label></td><td><input type="checkbox" checked="checked" name="remember" id="remember" />
+<label for="remember">Remember me</label></td><td><input type="checkbox" checked="checked" name="remember" id="remember" tabindex="3"/>
 </td></tr>
 </table>
-<input type="submit" name="login" value="Log in" />
+<input type="submit" name="login" value="Log in" tabindex="4"/>
 </form>
-<p>Activation email not received? <a href="index.php?show=notreceived" >Click here</a> to have it resent!</p>
+<p>Activation email not received? <a href="index.php?show=notreceived" tabindex="5" >Click here</a> to have it resent!</p>
