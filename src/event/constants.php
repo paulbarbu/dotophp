fd53154b2fa8fc6d97b7ff28865c0c2d765131ad
Barbu Paul - Gheorghe <paullik.paul@gmail.com>
2011-12-11 22:10:34 +0200

    manage events module's content with the form recovery

---
+++ src/event/constants.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @file src/event/constants.php
+ * @brief Manage events content
+ * @author Paul Barbu
+ *
+ * @ingroup eventFiles
+ */
+
+/**
+ * Repeat list
+ */
+$REPEATS = array(
+    '0' => 'Never',
+    '1' => 'Daily',
+    '2' => 'Weekly',
+    '3' => 'Monthly',
+    '4' => 'Yearly',
+);
+
+/**
+ * Priority list
+ */
+$PRIORITIES = array(
+    '0' => 'Low',
+    '1' => 'Medium',
+    '2' => 'High',
+    '3' => 'Urgent',
+);
---
+++ src/event/content.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @file src/event/content.php
+ * @brief Manage events content
+ * @author Paul Barbu
+ *
+ * @ingroup eventFiles
+ */
+
+/**
+ * @defgroup eventFiles Manage events
+ */
+
+require 'constants.php';
+
+?>
+<form action="" method="post">
+<fieldset id="q-add"> <legend>Quick add an event</legend>
+<input type="text" maxlength="20" name="quickadd" id="quick-add" tabindex="1"
+ <?php echo isset($_POST['quickadd']) ? 'value="' . $_POST['quickadd'] . '"' : NULL ?> />
+<input type="submit" name="quick-add" value="Add" tabindex="2" />
+</fieldset>
+
+<fieldset id="d-add"> <legend>Add an event specifying details</legend>
+<table  border="0" cellspacing="5"><tr><td>
+
+<label for="name">Name:</label></td><td><input type="text" maxlength="20" name="name" id="name" tabindex="3"
+ <?php echo isset($_POST['name']) ? 'value="' . $_POST['name'] . '"' : NULL ?> />
+</td><td>
+<label for="color">Color:</label></td><td><input type="text" maxlength="7" tabindex="7" name="color" id="color"
+value="<?php echo isset($_POST['color']) ? $_POST['color'] : '#RRGGBB' ?>" />
+</td></tr><tr><td rowspan="2">
+<label for="desc">Description:</label></td><td rowspan="2"><textarea tabindex="4" rows="3" cols="23" name="description" id="desc">
+<?php echo isset($_POST['description']) ? $_POST['description'] : NULL ?></textarea>
+</td><td>
+<label for="startdate">Start date:</label></td><td><input type="text" name="startdate" id="startdate" tabindex="8" maxlength="10"
+ <?php echo isset($_POST['startdate']) ? 'value="' . $_POST['startdate'] . '"' : 'value="' . DATE_TOUSER . '"' ?> />
+</td></tr><tr><td>
+<label for="enddate">Due for:</label></td><td><input type="text" name="enddate" id="enddate" tabindex="9" maxlength="10"
+ <?php echo isset($_POST['enddate']) ? 'value="' . $_POST['enddate'] . '"' : 'value="' . DATE_TOUSER . '"' ?> />
+</td></tr><tr><td>
+<label for="repeat">Repeat:</label></td><td><select tabindex="5" name="repeat" id="repeat">
+ <?php echo arrayToOption(array_values($REPEATS), array_keys($REPEATS), isset($_POST['repeat']) ? $_POST['repeat'] : NULL); ?>
+</select>
+</td><td>
+<label for="priv">Private event:</label></td><td><input id="priv" type="checkbox" name="private" tabindex="10" value="1"
+ <?php echo isset($_POST['private']) ? 'checked="checked"' : NULL ?> />
+</td></tr><tr><td>
+<label for="priority">Priority:</label></td><td><select tabindex="6" name="priority" id="priority">
+ <?php echo arrayToOption(array_values($PRIORITIES), array_keys($PRIORITIES), isset($_POST['priority']) ? $_POST['priority'] : NULL); ?>
+</select>
+</td><td>
+<label for="exception">Exception:</label></td><td><input id="exception" type="checkbox" name="exception" tabindex="11" value="1"
+ <?php echo isset($_POST['exception']) ? 'checked="checked"' : NULL ?> />
+</td></tr><tr><td colspan="4"><center>
+<input type="submit" name="add" value="Add event" tabindex="12"/>
+</center></td></tr></table>
+</fieldset>
+</form>
