a9d909761f45ec3c524d4706982fc1764bfc2bd4
Barbu Paul - Gheorghe <paullik.paul@gmail.com>
2011-12-22 20:08:56 +0200

    tabindexed the category form

--- src/category/content.php
+++ src/category/content.php
@@ -13,18 +13,18 @@ require 'constants.php';
 <form action="" method="post">
 <table border="0" cellspacing="5">
 <tr><td>
-<label for="name">Name:</label></td><td><input type="text" maxlength="20" name="name" id="name" tabindex="3"
+<label for="name">Name:</label></td><td><input type="text" maxlength="20" name="name" id="name" tabindex="1"
  <?php echo isset($_POST['name']) ? 'value="' . $_POST['name'] . '"' : NULL ?> /></td><td>
 
-<label for="desc">Description:</label></td><td><textarea tabindex="4" rows="3" cols="23" name="description" id="desc">
+<label for="desc">Description:</label></td><td><textarea tabindex="2" rows="3" cols="23" name="description" id="desc">
 <?php echo isset($_POST['description']) ? $_POST['description'] : NULL ?></textarea></td></tr>
 
 <tr><td>
-<label for="color">Color:</label></td><td><input type="text" maxlength="7" tabindex="7" name="color" id="color"
+<label for="color">Color:</label></td><td><input type="text" maxlength="7" tabindex="3" name="color" id="color"
 value="<?php echo isset($_POST['color']) ? $_POST['color'] : '#RRGGBB' ?>" /></td><td>
 
-<label for="repeat">Repeat:</label></td><td><select tabindex="5" name="repeat" id="repeat">
+<label for="repeat">Repeat:</label></td><td><select tabindex="4" name="repeat" id="repeat">
  <?php echo arrayToOption(array_values($REPEATS), array_keys($REPEATS), isset($_POST['repeat']) ? $_POST['repeat'] : NULL); ?>
 </select></td></tr><tr><td colspan="4"><center>
-<input type="submit" name="add" value="Add" /></center></td></tr>
+<input type="submit" name="add" value="Add" tabindex="5" /></center></td></tr>
 </form>
