ad0a31c2b7b9c2a103a25b2658afc5a2aa1323cb
Barbu Paul - Gheorghe <paullik.paul@gmail.com>
2011-12-27 13:12:45 +0200

    verifying the user submitted data and inserting it into the DB

--- src/category/category.php
+++ src/category/category.php
@@ -13,25 +13,56 @@
 
 if(isset($_POST['add'])){
     $err = array();
+    $retval = NULL;
+    $data = array();
 
     list($name, $description, $color, $repeat) = filterInput($_POST['name'],
         $_POST['description'], $_POST['color'], $_POST['repeat']);
 
-    if(empty($name)){
+    if(empty($name) || !isValidNick($name)){
         $err[] = C_ERR_NO_NAME;
     }
 
-    if(!isValidDesc($description)){
-        $err[] = C_ERR_DESC;
+    if(!empty($description)){
+        if(!isValidDesc($description)){
+            $err[] = C_ERR_DESC;
+        }
+        else{
+            $data['description'] = $description;
+        }
     }
 
-    if(!isValidColor($color)){
-        $err[] = C_ERR_COLOR;
+    if(!empty($color) && COLOR_CODE != $color){
+        if(!isValidColor($color)){
+            $err[] = C_ERR_COLOR;
+        }
+        else{
+            $data['color'] = $color;
+        }
     }
 
     if(!empty($err)){
         return $err;
     }
 
-    //continue here
+    $data['user_id'] = $_SESSION['uid'];
+    $data['name'] = $name;
+    $data['repeat_interval'] = $repeat;
+
+    if(!$feedback_pre['connect']){
+        $retval = C_ERR_DB_CONN;
+    }
+    else if(insertIntoDB($feedback_pre['connect'], 'category', array())){
+        $retval = ERR_NONE;
+    }
+    else{
+        $retval = C_ERR_DB;
+    }
+
+    if($retval == C_ERR_DB || $retval == C_ERR_DB_CONN){
+        writeLog($config['logger']['category'], '(' . mysqli_errno($feedback_pre['connect'])
+                 . ') ' . mysqli_error($feedback_pre['connect']) . PHP_EOL);
+    }
 }
+
+return TRUE;
