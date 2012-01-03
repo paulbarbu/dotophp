d536828d41b0962a706bc553b89d6d07991025d8
Barbu Paul - Gheorghe <paullik.paul@gmail.com>
2012-01-03 21:16:15 +0200

    re-tabindex'ed

--- src/event/content.php
+++ src/event/content.php
@@ -37,13 +37,13 @@ foreach($cat as $i){
 <label for="name" title="<?php echo TOOLTIP_NICK_CAT_EV ?>"> Name:</label></td><td><input title="<?php echo TOOLTIP_NICK_CAT_EV ?>" type="text" maxlength="20" name="name" id="name" tabindex="3"
  <?php echo isset($_POST['name']) ? 'value="' . $_POST['name'] . '"' : NULL ?> />
 </td><td>
-<label for="color">Color:</label></td><td><input type="text" maxlength="7" tabindex="7" name="color" id="color"
+<label for="color">Color:</label></td><td><input type="text" maxlength="7" tabindex="8" name="color" id="color"
 value="<?php echo isset($_POST['color']) ? $_POST['color'] : '#RRGGBB' ?>" />
-</td></tr><tr><td rowspan="2">
-<label for="desc" title="<?php echo TOOLTIP_DESC ?>">Description:</label></td><td rowspan="2"><textarea title="<?php echo TOOLTIP_DESC ?>" tabindex="4" rows="3" cols="23" name="description" id="desc">
+</td></tr><tr><td>
+<label for="desc" title="<?php echo TOOLTIP_DESC ?>">Description:</label></td><td><textarea title="<?php echo TOOLTIP_DESC ?>" tabindex="4" rows="2" cols="23" name="description" id="desc">
 <?php echo isset($_POST['description']) ? $_POST['description'] : NULL ?></textarea>
 </td><td>
-<label for="startdate">Start date:</label></td><td><input type="text" name="startdate" id="startdate" tabindex="8" maxlength="10"
+<label for="startdate">Start date:</label></td><td><input type="text" name="startdate" id="startdate" tabindex="9" maxlength="10"
  <?php echo isset($_POST['startdate']) ? 'value="' . $_POST['startdate'] . '"' : 'value="' . DATE_TOUSER . '"' ?> />
 </td></tr><tr>
 <td><label for='cat'>Category: </label></td><td><select name="cat" id ="cat" tabindex="5" >
@@ -53,21 +53,21 @@ value="<?php echo isset($_POST['color']) ? $_POST['color'] : '#RRGGBB' ?>" />
 <label for="enddate">Due for:</label></td><td><input type="text" name="enddate" id="enddate" tabindex="10" maxlength="10"
  <?php echo isset($_POST['enddate']) ? 'value="' . $_POST['enddate'] . '"' : 'value="' . DATE_TOUSER . '"' ?> />
 </td></tr><tr><td>
-<label for="repeat">Repeat:</label></td><td><select tabindex="5" name="repeat" id="repeat">
- <?php echo arrayToOption(array_values($REPEATS), array_keys($REPEATS), isset($_POST['repeat']) ? $_POST['repeat'] : NULL); ?>
+<label for="repeat">Repeat:</label></td><td><select tabindex="6" name="repeat" id="repeat">
+ <?php arrayToOption(array_values($REPEATS), array_keys($REPEATS), isset($_POST['repeat']) ? $_POST['repeat'] : NULL); ?>
 </select>
 </td><td>
-<label for="priv">Private event:</label></td><td><input id="priv" type="checkbox" name="private" tabindex="10" value="1"
+<label for="priv">Private event:</label></td><td><input id="priv" type="checkbox" name="private" tabindex="11" value="1"
  <?php echo isset($_POST['private']) ? 'checked="checked"' : NULL ?> />
 </td></tr><tr><td>
-<label for="priority">Priority:</label></td><td><select tabindex="6" name="priority" id="priority">
- <?php echo arrayToOption(array_values($PRIORITIES), array_keys($PRIORITIES), isset($_POST['priority']) ? $_POST['priority'] : NULL); ?>
+<label for="priority">Priority:</label></td><td><select tabindex="7" name="priority" id="priority">
+ <?php arrayToOption(array_values($PRIORITIES), array_keys($PRIORITIES), isset($_POST['priority']) ? $_POST['priority'] : NULL); ?>
 </select>
 </td><td>
-<label for="exception">Exception:</label></td><td><input id="exception" type="checkbox" name="exception" tabindex="11" value="1"
+<label for="exception">Exception:</label></td><td><input id="exception" type="checkbox" name="exception" tabindex="12" value="1"
  <?php echo isset($_POST['exception']) ? 'checked="checked"' : NULL ?> />
 </td></tr><tr><td colspan="4"><center>
-<input type="submit" name="add" value="Add event" tabindex="12"/>
+<input type="submit" name="add" value="Add event" tabindex="13"/>
 </center></td></tr></table>
 </fieldset>
 </form>
